07966d15525c6fb5f563f0dc06c5cb90fa22f4eb
Jerzy <jgrynczewski@gmail.com>
2020-10-24 09:47:53 +0200

    memoizacja

---
+++ 2_recursion/3_memoization.py
@@ -0,0 +1,40 @@
+def fib(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fib(n-1)+fib(n-2)
+#
+# for i in range(1, 101):
+#     print(f"fib({i}) = {fib(i)}")
+
+fib_cache = {}
+
+def fib_memo(n):
+    if n in fib_cache:
+        return fib_cache[n]
+    if n==0:
+        result = 0
+    elif n==1:
+        result = 1
+    else:
+        result = fib_memo(n-1) + fib_memo(n-2)
+    fib_cache[n] = result
+    return result
+#
+# for i in range(1, 201):
+#     print(f"fib({i}) = {fib_memo(i)}")
+
+from functools import lru_cache
+
+@lru_cache(maxsize=100)
+def fib(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+
+for i in range(1, 201):
+    print(f"fib({i}) = {fib_memo(i)}")
